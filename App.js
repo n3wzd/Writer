4d04e71d7b6706ea21839141dc35d46c5fe9bb36
n3wzd <87504447+n3wzd@users.noreply.github.com>
2024-02-11 21:29:47 +0900

    update MarkTree

--- App.js
+++ App.js
@@ -17,22 +17,22 @@ function Center() {
 
 function CenterTitle() {
   let text = defaultTitle;
-  return <div contenteditable='true'><h1>{text}</h1></div>
+  return <div contentEditable='true'><h1>{text}</h1></div>
 }
 
 function CenterContent() {
 	const [content, setContent] = useState('');
-	const divRef = useRef();
+	const divHTMLRef = useRef();
 
 	useEffect(() => {
-		if (divRef.current) {
-			divRef.current.innerHTML = setContent;
+		if (divHTMLRef.current) {
+			divHTMLRef.current.innerText = content;
 		}
 	}, [content]);
 	
 	const handleInputChange = (event) => {
 		const newText = event.target.innerText;
-		setContent(createHTMLByMarkRangePattern(newText));
+		setContent(createHTMLByTextContent(newText));
 	};
 
 	return (
@@ -58,8 +58,8 @@ const patternComment = '\\';
 
 function convertPatternToTag(pattern) {
 	switch(pattern) {
-		case '---': return 'h1';
-		case '```': return 'i';
+		// case '---': return 'h1';
+		// case '```': return 'i';
 		case '# ': return 'h1';
 		case '## ': return 'h2';
 		case '### ': return 'h3';
@@ -71,7 +71,9 @@ function convertPatternToTag(pattern) {
 		case '*': 
 		case '__': return 'i';
 		case '~~': return 's';
-		case '`': return 'b';
+		// case '`': return 'b';
+		case 'p': return 'p';
+		case 'br': return 'br';
 		default: return 'span';
 	}
 }
@@ -91,31 +93,69 @@ class MarkTreeNode {
 	get isChildrenEmpty() {
 		return this.children.length <= 0;
 	}
-	
 	get leftmostChildTagPos() {
-		return this.isChildrenEmpty ? this.lo : this.children[0].lo;
+		return this.isChildrenEmpty ? this.hi : this.children[0].lo;
 	}
-	
 	get rightmostChildTagPos() {            
-		return this.isChildrenEmpty ? this.hi : this.children[this.children.length - 1].hi;
+		return this.isChildrenEmpty ? this.lo : this.children[this.children.length - 1].hi;
+	}
+	get leftPattern() {
+		switch(this.pattern) {
+			case '# ': 
+			case '## ':
+			case '### ': 
+			case '#### ': 
+			case '##### ': 
+			case '###### ': 
+			case '**': 
+			case '_': 
+			case '*': 
+			case '__': 
+			case '~~': return this.pattern;
+			case 'p': 
+			case 'br': 
+			default: return '';
+		}
+	}
+	get rightPattern() {
+		switch(this.pattern) {
+			case '# ': 
+			case '## ':
+			case '### ': 
+			case '#### ': 
+			case '##### ': 
+			case '###### ': return '';
+			case '**': 
+			case '_': 
+			case '*': 
+			case '__': 
+			case '~~': return this.pattern;
+			case 'p': 
+			case 'br': 
+			default: return '';
+		}
 	}
 }
 
 function createRangeMarkList(textContent) {
 	const searchedMarkList = [];
-	
 	class SingleMarkElement {
 		constructor(pos, pattern) {
 			this.pos = pos;
 			this.pattern = pattern;
 		}
 	}
-
-	class markElement {
+	class MarkElement {
 		constructor(lo, hi, pattern) {
 			this.lo = lo;
 			this.hi = hi;
 			this.pattern = pattern;
+		}
+	}
+	class Range {
+		constructor(lo, hi) {
+			this.lo = lo;
+			this.hi = hi;
 		}
 	}
 	
@@ -135,7 +175,7 @@ function createRangeMarkList(textContent) {
 
 			if(matched) {
 				searchedMarkList.push(
-					new markElement(
+					new MarkElement(
 						singleMarkCharStack[idx].pos,
 						item.pos + item.pattern.length,
 						item.pattern
@@ -178,19 +218,51 @@ function createRangeMarkList(textContent) {
 	
 	const lines = textContent.split('\n');
 	const singleMarkCodeBlockStack = [];
+	const lineBrStack = [];
 	let lineOffset = 0;
+	function unleashBrStack() {
+		const length = lineBrStack.length;
+		for(let i = 1; i < length; i++) {
+			searchedMarkList.push(
+				new MarkElement(
+					lineBrStack[i].lo - 1,
+					lineBrStack[i].lo,
+					'br'
+				)
+			);
+		}
+		if(length > 0) {
+			searchedMarkList.push(
+			new MarkElement(
+					lineBrStack[0].lo,
+					lineBrStack[length - 1].hi,
+					'p'
+				)
+			);
+		}
+		lineBrStack.length = 0;
+	}
+	
 	for (const line of lines) {
+		if(line.length === 0) {
+			unleashBrStack();
+			lineOffset += 1;
+			continue;
+		}
+		
+		let patternMatched = false;
 		let skipPos = 0;
 		if(patternOnlyLine.length === line.length) {
 			const lineSeg = line.substring(0, patternOnlyLine.length);
 			if(patternOnlyLine === lineSeg) {
 				searchedMarkList.push(
-					new markElement(
+					new MarkElement(
 						lineOffset,
 						lineOffset + line.length,
 						patternOnlyLine
 					)
 				);
+				patternMatched = true;
 				skipPos += line.length;
 			}
 		}
@@ -200,7 +272,7 @@ function createRangeMarkList(textContent) {
 			if(patternCodeBlock === lineSeg) {
 				if(singleMarkCodeBlockStack.length > 0) {
 					searchedMarkList.push(
-						new markElement(
+						new MarkElement(
 							singleMarkCodeBlockStack.pop(),
 							lineOffset + line.length,
 							patternCodeBlock
@@ -209,6 +281,7 @@ function createRangeMarkList(textContent) {
 				} else {
 					singleMarkCodeBlockStack.push(lineOffset);
 				}
+				patternMatched = true;
 				skipPos += line.length;
 			}
 		}
@@ -221,27 +294,35 @@ function createRangeMarkList(textContent) {
 			const lineSeg = line.substring(0, patternLength);
 			if(pattern === lineSeg) {
 				searchedMarkList.push(
-					new markElement(
+					new MarkElement(
 						lineOffset,
 						lineOffset + line.length,
 						pattern
 					)
 				);
+				patternMatched = true;
 				skipPos += patternLength;
 				break;
 			}
 		}
 		
 		scanLine(line, lineOffset, skipPos);
+		if(patternMatched) {
+			unleashBrStack();
+		} else {
+			lineBrStack.push(new Range(lineOffset, lineOffset + line.length));
+		}
 		lineOffset += line.length + 1;
 	}
-	searchedMarkList.sort((x, y) => x.lo - y.lo);
+	unleashBrStack();
+	
+	searchedMarkList.sort((x, y) => x.lo !== y.lo ? x.lo - y.lo : (x.pattern === 'p' ? -1 : 1));
 	return searchedMarkList;
 }
 
 function createMarkTree(textContent) {
 	const searchedMarkList = createRangeMarkList(textContent);
-	const rootNode = new MarkTreeNode(0, textContent.length, '');
+	const rootNode = new MarkTreeNode(0, textContent.length, 'div');
 	const nodeStack = [];
 	
 	let curNode = rootNode;
@@ -255,14 +336,44 @@ function createMarkTree(textContent) {
 		nodeStack.push(curNode);
 		curNode = newNode;
 	}
-	
 	return rootNode;
 }
 
-function createHTMLByMarkRangePattern(text) {
-	const rootNode = createMarkTree(text);
-	console.log(rootNode);
+function createHTMLByTree(rootNode, text) {
 	function scanTree(curNode) {
+		if(curNode.pattern === 'br') {
+			return React.createElement(convertPatternToTag(curNode.pattern));
+		}
+		
+		const children = [];
+		for(let i = 0; i < curNode.children.length; i++) {
+			if(i > 0) {
+				children.push(text.substring(curNode.children[i - 1].hi, curNode.children[i].lo));
+			}
+			children.push(scanTree(curNode.children[i]));
+		}
+		
+		const leftPattern = curNode.leftPattern;
+		const rightPattern = curNode.rightPattern;
+		let leftTextTag = '', rightTextTag = '';
+		if(curNode.isChildrenEmpty) {
+			children.push(text.substring(curNode.lo + leftPattern.length, curNode.hi - rightPattern.length));
+		} else {
+			leftTextTag = text.substring(curNode.lo + leftPattern.length, curNode.leftmostChildTagPos);
+			rightTextTag = text.substring(curNode.rightmostChildTagPos, curNode.hi - rightPattern.length);
+		}
+		return React.createElement(convertPatternToTag(curNode.pattern), null, [leftTextTag].concat(children, [rightTextTag]));
+	}
+	return scanTree(rootNode);
+}
+
+// Not Complete
+function createEditorTextByTree(rootNode, text) {
+	function scanTree(curNode) {
+		if(curNode.tag === 'br') {
+			return React.createElement(curNode.tag);
+		}
+		
 		const children = [];
 		for(let i = 0; i < curNode.children.length; i++) {
 			if(i > 0) {
@@ -274,11 +385,20 @@ function createHTMLByMarkRangePattern(text) {
 			children.push(text.substring(curNode.lo, curNode.hi));
 		}
 		
-		const leftChild = text.substring(curNode.lo, curNode.leftmostChildTagPos);
-		const rightChild = text.substring(curNode.rightmostChildTagPos, curNode.hi);
-		const tag = convertPatternToTag(curNode.pattern);
-		return React.createElement(tag, null, [leftChild].concat(children, [rightChild]));
+		const leftPattern = curNode.leftPattern;
+		const rightPattern = curNode.rightPattern;
+		const leftTextTag = text.substring(curNode.lo, curNode.leftmostChildTagPos);
+		const rightTextTag = text.substring(curNode.rightmostChildTagPos, curNode.hi);
+		const leftPatternTag = React.createElement('b', null, leftPattern);
+		const rightPatternTag = React.createElement('b', null, rightPattern);
+		return React.createElement(convertPatternToTag(curNode.tag), null, [leftPatternTag, leftTextTag].concat(children, [rightTextTag, rightPatternTag]));
 	}
-	const HTML = scanTree(rootNode);
+	return scanTree(rootNode);
+}
+
+function createHTMLByTextContent(text) {
+	const rootNode = createMarkTree(text);
+	const HTML = createHTMLByTree(rootNode, text);
+	const editorText = createEditorTextByTree(rootNode, text);
 	return HTML;
 }
